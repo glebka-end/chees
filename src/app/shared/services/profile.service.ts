cf742affcee6152816e0c5be5db525088339d734
glebka-end <pytko.gleba@gmail.com>
2023-06-28 13:26:27 +0300

    проблема с отправкой на сер ) рег раб

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -14,17 +14,21 @@ class interfrofil {
   providedIn: 'root'
 })
 export class ProfileService {
-  private token =''//
-  private a =''
+  // const token = localStorage.getItem('auth-token');
 
+  private a =''
+  private token: string;
   constructor(private http: HttpClient) {
+    this.token = localStorage.getItem('auth-token') || '';
   }
   //ate (interfrofil: Interfrofil): Observable <interfrofil> {
   //    return this.hhtp.post<interfrofil>('http://127.0.0.1:/api/users/users/update-profile',interfrofil)
   // }
   sendData(data: any) {
-    const url = 'http://127.0.0.1:80/api/comment/{commentId}/likes'; // замените на свой URL
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
+
+    // console.log(this.token)
+    const url = 'http://127.0.0.1:80/api/users/users/update-profile'; // замените на свой URL
+    const headers = new HttpHeaders().set('Authorization', `auth-token ${this.token}`);
     const options = { headers: headers };
     return this.http.put(url, data);
   }
